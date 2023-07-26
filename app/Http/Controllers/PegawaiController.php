0919970eac8b0cddc65e8e846e55124e325f6127
gustigiyus <gustiggt123@gmail.com>
2023-07-26 14:31:22 +0700

    Laravel Data

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pegawai;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class PegawaiController extends Controller
+{
+    public function index()
+    { 
+        $itemsPerPage = 5;
+        $pegawai_dt = Pegawai::orderBy('updated_at', 'desc')->paginate($itemsPerPage);
+        
+        $no = ($pegawai_dt->currentPage() - 1) * $itemsPerPage + 1;
+        $data = [
+            'title' => 'Pegawai',
+            'pegawai' => $pegawai_dt,
+            'no' => $no
+        ];
+                
+        return view('pegawai.index', $data);
+    }
+
+    public function detail($id) 
+    {
+        $pegawai_dt = Pegawai::where('nomer_induk', $id)->first();
+        $data = [
+            'title' => 'Pegawai Detail',
+            'pegawai' => $pegawai_dt
+        ];
+                
+        return view('pegawai.show', $data);
+    }
+
+    public function create() 
+    {
+        $data = [
+            'title' => 'Form Pegawai',
+        ];
+        return view('pegawai.create', $data);
+    }
+
+    public function store(Request $request) 
+    {
+
+        // SESSION FLASH VALUE DEFAULT
+        Session::flash('pegawai_nama', $request->pegawai_nama);
+        Session::flash('pegawai_jabatan', $request->pegawai_jabatan);
+        Session::flash('pegawai_umur', $request->pegawai_umur);
+        Session::flash('pegawai_alamat', $request->pegawai_alamat);
+
+        $data = [
+            'pegawai_nama' => $request->input('pegawai_nama'),
+            'pegawai_jabatan' => $request->input('pegawai_jabatan'),
+            'pegawai_umur' => $request->input('pegawai_umur'),
+            'pegawai_alamat' => $request->input('pegawai_alamat')
+        ];
+
+        Pegawai::create($data);
+
+        return redirect('pegawai')->with('success', 'Data berhasil ditambahkan');
+    }
+
+    public function edit($id) 
+    {
+        $pegawai_dt = Pegawai::where('id', $id)->first();
+        $data = [
+            'title' => 'Form Edit Pegawai',
+            'pegawai' => $pegawai_dt
+        ];
+
+        return view('pegawai.edit', $data);
+    }
+
+    public function update(Request $request, $id) 
+    {
+        $request->validate([
+            'pegawai_nama' => 'required',
+            'pegawai_jabatan' => 'required',
+            'pegawai_umur' => 'required',
+            'pegawai_alamat' => 'required',
+        ],[
+            'pegawai_nama.required' => 'Nama wajib diisi',
+            'pegawai_jabatan.required' => 'jabatan wajib diisi',
+            'pegawai_umur.required' => 'umur wajib diisi',
+            'pegawai_alamat.required' => 'alamat wajib diisi',            
+        ]);
+
+        $data = [
+            'pegawai_nama' => $request->input('pegawai_nama'),
+            'pegawai_jabatan' => $request->input('pegawai_jabatan'),
+            'pegawai_umur' => $request->input('pegawai_umur'),
+            'pegawai_alamat' => $request->input('pegawai_alamat')
+        ];
+
+        Pegawai::where('id', $id)->update($data);
+        return redirect('pegawai')->with('success', 'Data berhasil diedit');
+    }
+
+    public function destroy($id) 
+    {
+        Pegawai::where('id', $id)->delete();
+        return redirect('pegawai')->with('success', 'Data berhasil dihapus');
+    }
+}
---
+++ database/migrations/2023_07_26_062028_create_pegawai_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->id();
+            $table->string('pegawai_nama')->required();
+            $table->string('pegawai_jabatan')->required();
+            $table->bigInteger('pegawai_umur')->required();
+            $table->text('pegawai_alamat')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+};
